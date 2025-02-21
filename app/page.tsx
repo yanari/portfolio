f624e83fcbffdddff22451a8e4fb44d0da9d9c79
Marcelle Yanari <yanarimy@gmail.com>
2025-02-21 20:40:20 -0300

    highlight.js

--- app/page.tsx
+++ app/page.tsx
@@ -7,45 +7,49 @@ import dynamic from "next/dynamic";
 import { useVisibleSection } from "@/hooks";
 
 const Components = {
-  about: dynamic(() => import("../sections/About"), {
-    loading: () => <p>Loading...</p>,
-  }),
-  projects: dynamic(() => import("../sections/Projects"), {
-    loading: () => <p>Loading...</p>,
-  }),
-  skills: dynamic(() => import("../sections/Skills"), {
-    loading: () => <p>Loading...</p>,
-  }),
-  experience: dynamic(() => import("../sections/Experience"), {
-    loading: () => <p>Loading...</p>,
-  }),
-  contact: dynamic(() => import("../sections/Contact"), {
-    loading: () => <p>Loading...</p>,
-  }),
+    about: dynamic(() => import("../sections/About"), {
+        loading: () => <p>Loading...</p>,
+    }),
+    projects: dynamic(() => import("../sections/Projects"), {
+        loading: () => <p>Loading...</p>,
+    }),
+    skills: dynamic(() => import("../sections/Skills"), {
+        loading: () => <p>Loading...</p>,
+    }),
+    experience: dynamic(() => import("../sections/Experience"), {
+        loading: () => <p>Loading...</p>,
+    }),
+    contact: dynamic(() => import("../sections/Contact"), {
+        loading: () => <p>Loading...</p>,
+    }),
 };
 
 export default function Home() {
-  const visibleSection = useVisibleSection();
-  return (
-    <>
-      <Navigation menuItems={SectionData} visibleSection={visibleSection} />
-      <main className="bg-gray-900">
-        {SectionData.map((data, index) => {
-          const Component = Components[data.id];
-          const color = index % 2 === 0 ? "bg-gray-800" : "";
+    const visibleSection = useVisibleSection();
+    return (
+        <>
+            <Navigation
+                menuItems={SectionData}
+                visibleSection={visibleSection}
+            />
+            <main className='bg-slate-800'>
+                {SectionData.map((data, index) => {
+                    const Component = Components[data.id];
+                    // TODO: select theme: vscode themes and backgrounds
+                    const color = index % 2 === 0 ? "bg-tokyo-night-dark" : "";
 
-          return (
-            <section
-              id={data.id}
-              data-section
-              className={`${color} pl-navBarMobile md:pl-80 m-auto text-3xl h-screen`}
-              key={data.id}
-            >
-              <Component />
-            </section>
-          );
-        })}
-      </main>
-    </>
-  );
+                    return (
+                        <section
+                            id={data.id}
+                            data-section
+                            className={`${color} pl-navBarMobile md:pl-80 m-auto text-3xl h-screen`}
+                            key={data.id}
+                        >
+                            <Component />
+                        </section>
+                    );
+                })}
+            </main>
+        </>
+    );
 }
--- sections/About.tsx
+++ sections/About.tsx
@@ -1,7 +1,28 @@
+import { useEffect } from 'react';
+import hljs from 'highlight.js';
+import javascript from 'highlight.js/lib/languages/javascript';
+
+import 'highlight.js/styles/tokyo-night-dark.css';
+
+hljs.registerLanguage('javascript', javascript);
+
+const javascriptCode = `const dev = {
+    name: 'Marcelle',
+    role: 'Frontend Developer',
+}
+`;
+
 export default function About() {
+    useEffect(() => {
+        hljs.initHighlighting();
+    }, []);
     return (
-        <div  className="text-gray-50 px-8 py-16 flex flex-col">
-           About
+        <div className="h-full text-gray-50 flex flex-col items-center justify-center">
+           <pre>
+                <code className="js">
+                    {javascriptCode}
+                </code>
+           </pre>
         </div>
     )
 }

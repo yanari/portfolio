3930a3998489fac88fdfda875e0f0b36f716d67a
Marcelle Yanari <yanarimy@gmail.com>
2025-04-17 01:59:41 -0300

    about page always h-dvh

--- app/(root)/sections/About.tsx
+++ app/(root)/sections/About.tsx
@@ -32,7 +32,7 @@ export default function About() {
     const isDesktop = width > 1024
 
     return (
-        <div className="text-lg md:text-xl lg:text-4xl h-full text-gray-50 flex flex-col items-center justify-center">
+        <div className="text-lg md:text-xl lg:text-4xl h-dvh text-gray-50 flex flex-col items-center justify-center">
             <pre className="justify-self-center h-70 md:h-90 md:w-3xl">
                 <TypewriterEffect code={isDesktop ? desktopText : mobileText} />
             </pre>
